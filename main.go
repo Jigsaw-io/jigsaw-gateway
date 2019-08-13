2db0a167308c21888b66bb070f620e7060b346a0
Zeemzo <azeemashraf@outlook.com>
2019-08-14 02:57:08 +0530

    cors fixed

--- main.go
+++ main.go
@@ -25,9 +25,10 @@ func main() {
 	// getEnvironment()
 
 	port := getPort()
-	headersOk := handlers.AllowedHeaders([]string{"Content-Type"})
-	originsOk := handlers.AllowedOrigins([]string{"*"})
+	headersOk := handlers.AllowedHeaders([]string{"Content-Type","Access-Control-Allow-Origin"})
+	originsOk := handlers.AllowedOrigins([]string{"https://jigsaw-io.firebaseapp.com","http://localhost:3000","https://jigsaw.cf"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	
 
 	router := routes.NewRouter()
 	fmt.Println("Jigsaw Gateway Started @port " + port)
